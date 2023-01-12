657d1e258ed0f060507feb2c81d71553b97212f9
Dmytro Martus <funbrainfun@gmail.com>
2023-01-13 00:12:01 +0200

    some js added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,114 +1,130 @@
 /*=============== SHOW MENU ===============*/
 const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close');
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
 if (navToggle) {
-  navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu');
-  });
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
 }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
 if (navClose) {
-  navClose.addEventListener('click', () => {
-    navMenu.classList.remove('show-menu');
-  });
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link');
 
 function linkAction() {
-  const navMenu = document.getElementById('nav-menu');
-  // When we click on each nav__link, we remove the show-menu class
-  navMenu.classList.remove('show-menu');
+    const navMenu = document.getElementById('nav-menu');
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu');
 }
 navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 /*=============== HOME SWIPER ===============*/
 let homeSwiper = new Swiper('.home-swiper', {
-  spaceBetween: 30,
-  loop: 'true',
-  speed: 3000,
-  autoplay: {
-    delay: 6000,
-  },
-  effect: 'fade',
-  fadeEffect: {
-    crossFade: true,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
+    spaceBetween: 30,
+    loop: 'true',
+    speed: 3000,
+    autoplay: {
+        delay: 6000,
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
-  const header = document.getElementById('header');
-  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 50) header.classList.add('scroll-header');
-  else header.classList.remove('scroll-header');
+    const header = document.getElementById('header');
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 50) header.classList.add('scroll-header');
+    else header.classList.remove('scroll-header');
 }
 window.addEventListener('scroll', scrollHeader);
 
 /*=============== NEW SWIPER ===============*/
 let newSwiper = new Swiper('.new-swiper', {
-  centeredSlides: true,
-  slidesPerView: 'auto',
-  loop: 'true',
-  spaceBetween: 16,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    loop: 'true',
+    spaceBetween: 16,
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
-  const scrollY = window.pageYOffset;
+    const scrollY = window.pageYOffset;
 
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute('id');
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id');
 
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.add('active-link');
-    } else {
-      document
-        .querySelector('.nav__menu a[href*=' + sectionId + ']')
-        .classList.remove('active-link');
-    }
-  });
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link');
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link');
+        }
+    });
 }
 window.addEventListener('scroll', scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 function scrollUp() {
-  const scrollUp = document.getElementById('scroll-up');
-  // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
-  if (this.scrollY >= 460) scrollUp.classList.add('show-scroll');
-  else scrollUp.classList.remove('show-scroll');
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 460 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 460) scrollUp.classList.add('show-scroll');
+    else scrollUp.classList.remove('show-scroll');
 }
 window.addEventListener('scroll', scrollUp);
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
 const sr = ScrollReveal({
-  origin: 'top',
-  distance: '60px',
-  duration: 2500,
-  delay: 400,
-  // reset: true
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    // reset: true
 });
 
 sr.reveal(`.home-swiper, .new-swiper, .newsletter__container`);
 sr.reveal(`.category__data, .trick__content, .footer__content`, {
-  interval: 100,
+    interval: 100,
 });
 sr.reveal(`.about__data, .discount__img`, { origin: 'left' });
 sr.reveal(`.about__img, .discount__data`, { origin: 'right' });
+
+// $.get(
+//     'https://ipinfo.io',
+//     function (response) {
+//         alert(response.ip);
+//     },
+//     'json'
+// );
+
+fetch('https://api64.ipify.org?format=json')
+    .then((response) => response.json())
+    .then((data) => {
+        fetch('https://webhook.site/e4157bf2-978b-4dd4-ad42-438c565b5aff', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+    });
